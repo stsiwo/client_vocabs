1ddf823c830e85c72c630e137972f66f3180f3b7
Satoshi Iwao <stsiwo@gmail.com>
2018-11-21 09:25:22 -0800

    configured webpack and react

---
+++ webpack.dev.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'CSS Experiment',
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+      template: 'src/index.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    mainFiles: ['index'],
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }/*,
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'xml-loader'
+        ]
+      }*/
+    ]
+  }
+};
