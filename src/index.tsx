c2cfeecc877fde1aa6ee81d4e66d531207cc1e3f
Satoshi Iwao <stsiwo@gmail.com>
2018-11-23 16:33:01 -0800

    config tslint and Syntastic

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,37 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-class MyComponent extends React.Component {
+interface Props {
+}
+
+interface State {
+  isLoading: boolean,
+  data: Data
+}
+
+interface Data {
+  name: string
+}
+
+class MyComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      data: { 
+        name: "default" 
+      }
+    }
+    this.handleFetch = this.handleFetch.bind(this);
+  }
+  handleFetch() {
+
+  } 
   render() {
-    return <div>Welcome to Client React Vocabs App</div>;
+    const { isLoading, data } = this.state;
+
+    if (isLoading) return <div>loading....</div>;
+    if (!isLoading) return <div>{data.name}</div>;
   }
 }
  
