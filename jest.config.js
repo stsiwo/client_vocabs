08f26a106ee0416ad1743817e4c9c63e6b0de0be
sts-msys <stsiwo@gmail.com>
2019-01-06 02:55:37 -0800

    add identity-object-proxy packge to fix jest assets import issues

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,9 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
+  "moduleNameMapper": {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "identity-obj-proxy"
+  },
   "testRegex": "\\.(spec|test)\\.tsx?$",
   "moduleFileExtensions": [
     "ts",
