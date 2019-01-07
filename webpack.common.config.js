88f8b25223cee625bb004f9d72d5772ffc788aa5
sts-msys <stsiwo@gmail.com>
2019-01-06 16:58:24 -0800

    added and configured 'typescript-plugin-styled-component' to display component name in chrome devtool

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+// 1. import default from the plugin module
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
+
+// 2. create a transformer;
+// the factory additionally accepts an options object which described below
+const styledComponentsTransformer = createStyledComponentsTransformer();
 
 module.exports = {
   entry: {
@@ -31,7 +37,12 @@ module.exports = {
           loader: 'babel-loader',
         }
       },
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { test: /\.tsx?$/, 
+        loader: "awesome-typescript-loader",
+        options: {
+          getCustomTransformers: () => ({ before: [styledComponentsTransformer] })
+        }
+      },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       {
         test: /\.css$/,
