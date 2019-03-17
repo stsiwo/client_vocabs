21d80a7980919cea8cff0318f9d23cc4f320eaea
sts-msys <stsiwo@gmail.com>
2019-03-17 12:39:01 -0700

    <TRUNCATESTRING>: added '...' to teh end of string

--- src/util/truncateString.ts
+++ src/util/truncateString.ts
@@ -1,6 +1,6 @@
 /**
  * truncate long string for ui
  **/
-const truncateString = ( input: string ): string => input.length > 20 ? input.substring(0, 19) : input;
+const truncateString = ( input: string ): string => input.length > 20 ? input.substring(0, 16) + "..." : input;
 export default truncateString;
 
